06d519fefdcfd5caa14331b2aa198193dc5939ac
suhee-cho <83660388+suhee-cho@users.noreply.github.com>
2026-04-17 16:18:38 +0000

    Deploying to gh-pages from @ suhee-cho/suhee-cho.github.io@0a525515f558f807affefbdf5355d2c4f436c5c0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,19 +9,33 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
+  },{id: "nav-news",
+          title: "News",
+          description: "Some small but exciting news in my life",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/news/";
+          },
+        },{id: "nav-blog",
           title: "Blog",
-          description: "",
+          description: "Random miscellaneous thoughts about research and life",
           section: "Navigation",
           handler: () => {
             window.location.href = "/blog/";
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "A growing collection of your cool projects.",
+          description: "Collection of my research projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
+          },
+        },{id: "nav-bookshelf",
+          title: "Bookshelf",
+          description: "사람은 책을 만들고 책은 사람을 만든다 - 신용호Books that I have read",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/books/";
           },
         },{id: "post-prologue",
         
@@ -31,7 +45,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2026/prologue/";
+            window.location.href = "/2026/02/20/prologue.html";
           
         },
       },{id: "post-between-idealism-and-reality",
@@ -42,14 +56,49 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2024/idealism/";
+            window.location.href = "/2024/02/02/idealism.html";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
+      },{id: "books-eleanor-oliphant-is-completely-fine",
+          title: 'Eleanor Oliphant is completely fine',
           description: "",
           section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
+              window.location.href = "/books/Eleanor_Oliphant.html";
+            },},{id: "books-i-39-m-glad-my-mom-died",
+          title: 'I&amp;#39;m glad my mom died',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/Im_glad_my_mom_died.html";
+            },},{id: "books-klala-and-the-sun",
+          title: 'Klala and the sun',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/Klara_and_the_sun.html";
+            },},{id: "books-born-a-crime",
+          title: 'Born a crime',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/born_a_crime.html";
+            },},{id: "books-why-fish-don-39-t-exist",
+          title: 'Why Fish Don&amp;#39;t Exist',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/fish_dont_exist.html";
+            },},{id: "books-hillbilly-elegy",
+          title: 'Hillbilly Elegy',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/hillbilly_elegy.html";
+            },},{id: "books-life-is-hard",
+          title: 'Life is hard',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/life_is_hard.html";
+            },},{id: "books-raising-hare",
+          title: 'Raising Hare',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/raising_hare.html";
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
@@ -57,7 +106,7 @@ ninja.data = [{
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
+              window.location.href = "/news/announcement_2.html";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
@@ -65,15 +114,15 @@ ninja.data = [{
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "teachings-data-science-fundamentals",
-          title: 'Data Science Fundamentals',
-          description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
-          section: "Teachings",handler: () => {
-              window.location.href = "/teachings/data-science-fundamentals/";
-            },},{id: "teachings-introduction-to-machine-learning",
-          title: 'Introduction to Machine Learning',
-          description: "This course provides an introduction to machine learning concepts, algorithms, and applications. Students will learn about supervised and unsupervised learning, model evaluation, and practical implementations.",
-          section: "Teachings",handler: () => {
-              window.location.href = "/teachings/introduction-to-machine-learning/";
+              window.location.href = "/projects/1_project.html";
+            },},{id: "projects-capturing-rapid-learning-in-an-extended-successor-representation-theory-of-the-cognitive-map",
+          title: 'Capturing Rapid Learning in an Extended Successor Representation Theory of the Cognitive Map...',
+          description: "A computational model showing how the hippocampus supports rapid, one-shot spatial learning via BTSP and perceived salience-weighted successor representations.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2_project.html";
+            },},{id: "projects-neuromimetic-metaplasticity-for-adaptive-continual-learning-without-catastrophic-forgetting",
+          title: 'Neuromimetic Metaplasticity for Adaptive Continual Learning without Catastrophic Forgetting',
+          description: "A brain-inspired synaptic metaplasticity model that enables deep neural networks to learn continuously without catastrophic forgetting, by intermixing stable and unstable synapses.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3_project.html";
             },},];
